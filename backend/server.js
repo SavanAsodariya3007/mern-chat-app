76877576e4b6833676191415902820eff4ac874c
IntuzSavanA <94033580+IntuzSavanA@users.noreply.github.com>
2022-04-08 15:06:49 +0530

    Back to orignal

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
@@ -11,38 +10,6 @@ const path = require("path");
 dotenv.config();
 connectDB();
 const app = express();
-
-const privateKey = fs.readFileSync(
-  "/etc/letsencrypt/live/chatapp.savan.tk/privkey.pem",
-  "utf8"
-);
-const certificate = fs.readFileSync(
-  "/etc/letsencrypt/live/chatapp.savan.tk/cert.pem",
-  "utf8"
-);
-const ca = fs.readFileSync(
-  "/etc/letsencrypt/live/chatapp.savan.tk/chain.pem",
-  "utf8"
-);
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
-
-const httpServer = http
-  .createServer(app)
-  .listen(
-    PORT,
-    console.log(`http Server running on PORT ${PORT}...`.yellow.bold)
-  );
-const httpsServer = https
-  .createServer(credentials, app)
-  .listen(
-    PORT,
-    console.log(`https Server running on PORT ${PORT}...`.yellow.bold)
-  );
 
 app.use(express.json()); // to accept json data
 
@@ -83,55 +50,15 @@ const server = app.listen(
   console.log(`Server running on PORT ${PORT}...`.yellow.bold)
 );
 
-const httpIO = require("socket.io")(httpServer, {
+const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chatapp.savan.tk",
+    origin: "http://chatapp.savn.tk",
     // credentials: true,
   },
 });
 
-httpIO.on("connection", (socket) => {
-  console.log("Connected to socket.io");
-  socket.on("setup", (userData) => {
-    socket.join(userData._id);
-    socket.emit("connected");
-  });
-
-  socket.on("join chat", (room) => {
-    socket.join(room);
-    console.log("User Joined Room: " + room);
-  });
-  socket.on("typing", (room) => socket.in(room).emit("typing"));
-  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
-  socket.on("new message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
-
-    if (!chat.users) return console.log("chat.users not defined");
-
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
-
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
-    });
-  });
-
-  socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
-    socket.leave(userData._id);
-  });
-});
-
-const httpsIO = require("socket.io")(httpsServer, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "https://chatapp.savan.tk",
-    // credentials: true,
-  },
-});
-
-httpsIO.on("connection", (socket) => {
+io.on("connection", (socket) => {
   console.log("Connected to socket.io");
   socket.on("setup", (userData) => {
     socket.join(userData._id);
